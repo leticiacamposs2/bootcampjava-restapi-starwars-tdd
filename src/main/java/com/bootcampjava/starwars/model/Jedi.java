36c520467dcbaaff1bd067f6102b7605c1d59fe2
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-03-22 21:05:22 -0300

    service, controller, models

--- src/main/java/com/bootcampjava/starwars/model/Jedi.java
+++ src/main/java/com/bootcampjava/starwars/model/Jedi.java
@@ -2,14 +2,14 @@ package com.bootcampjava.starwars.model;
 
 public class Jedi {
 
-    private int id;
+    private Integer id;
     private String name;
     private int strength;
     private int version;
 
     public Jedi() {}
 
-    public Jedi(int id, String name, int strength, int version) {
+    public Jedi(Integer id, String name, int strength, int version) {
         this.id = id;
         this.name = name;
         this.strength = strength;
@@ -21,35 +21,28 @@ public class Jedi {
         this.strength = strength;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getStrength() {
         return strength;
     }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
     public int getVersion() {
         return version;
     }
-
     public void setVersion(int version) {
         this.version = version;
+    }
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
     }
 }
